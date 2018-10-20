89e6f7cacee31643dc11cb2d2bc05504edf8c2c0
Miguel Vicente <mvicmoure@gmail.com>
2018-10-20 17:20:12 +0200

    Modelos de opcion y encuesta, y mapper de encuesta inicial.

---
+++ model/Option.php
@@ -0,0 +1,50 @@
+<?php
+
+class Option {
+    private $id, $survey_id, $day, $start, $end;
+
+    public function __construct($id = NULL, $survey_id = NULL, $day = NULL, $start = NULL, $end = NULL) {
+        $this->id = $id;
+        $this->survey_id = $survey_id;
+        $this->day = $day;
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getSurveyId() {
+        return $this->survey_id;
+    }
+
+    public function getDay() {
+        return $this->day;
+    }
+
+    public function setDay($day) {
+        $this->day = $day;
+    }
+
+    public function getStart() {
+        return $this->start;
+    }
+
+    public function setStart($start) {
+        $this->start = $start;
+    }
+
+    public function getEnd() {
+        return $this->end;
+    }
+
+    public function setEnd($end) {
+        $this->end = $end;
+    }
+
+    public function checkStartEnd() {
+        // TODO
+    }
+}
+?>
---
+++ model/SurveyMapper.php
@@ -0,0 +1,76 @@
+<?php
+require_once(__DIR__."/../core/PDOConnection.php");
+
+require_once(__DIR__."/../model/User.php");
+require_once(__DIR__."/../model/Option.php");
+require_once(__DIR__."/../model/Survey.php");
+
+class SurveyMapper {
+    private $db;
+
+    public function __construct() {
+        $this->db = PDOConnection::getInstance();
+    }
+
+    public function findByCreator($creatorid) {
+        $stmt = $this->db->prepare("SELECT 
+surveys.id, surveys.title, surveys.description,
+users.id, users.username
+FROM surveys JOIN users ON surveys.creator_id = users.id
+WHERE surveys.creator_id = ?");
+        $stmt->execute(array($creatorid));
+        $surveys = $stmt->fetch(PDO::FETCH_ASSOC);
+        $result = array();
+
+        foreach($surveys as $s) {
+            $survey_creator = new User($s["users.id"], $s["users.name"], $s["users.surname"], $s["users.email"], $s["users.pass"]);
+            $current_survey = new Survey($s["surveys.id"], $s["surveys.title"],
+            $s["surveys.description"], $survey_creator);
+            array_push($result, $current_survey);
+        }
+        
+        return $result;
+    }
+
+    public function findById($survey_id) {
+        $stmt = $this->db->prepare("SELECT surveys.id, surveys.title, surveys.description, users.id, users.name, users.surname, users.email, users.pass FROM surveys JOIN users ON surveys.creator_id = users.id WHERE surveys.id = ?");
+        $stmt->execute(array($survey_id));
+        $survey_row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($survey_row == NULL) {
+            return NULL;
+        }
+
+        $stmt = $this->db->prepare("SELECT * FROM options WHERE survey_id = ?");
+        $stmt->execute(array($survey_id));
+        $options_arr = $stmt->fetch(PDO::FETCH_ASSOC);
+        $options = array();
+        
+        foreach($options_arr as $option_row) {
+            $op = new Option($option_row["id"], $option_row["day"], $option_row["start"], $option_row["end"]);
+            array_push($options, $op);
+        }
+        
+        $creator = new User($survey_row["users.id"], $survey_row["users.name"], $survey_row["users.surname"], $survey_row["users.email"], $survey_row["users.pass"]);
+        $survey = new Survey($survey_row["surveys.id"], $survey_row["surveys.title"], $survey_row["surveys.description"], $creator, $options);
+        return $survey;
+    }
+
+    public function save(Survey $survey) {
+        $stmt = $this->db->prepare("INSERT INTO surveys(title, description, creator_id) VALUES (?, ?, ?)");
+        $stmt->execute(array($survey->getTitle(), $survey->getDescription(), $survey->getCreator()->getId()));
+        return $this->db->lastInsertId();
+    }
+
+    public function update(Survey $survey) {
+        $stmt = $this->db->prepare("UPDATE surveys SET title=?, description=?, creator_id = ? WHERE id = ?");
+        $stmt->execute(array($survey->getTitle(), $survey->getDescription(), $survey->getCreator()->getId(), $survey->getId()));
+    }
+
+    public function delete(Survey $survey) {
+        $stmt = $this->db->prepare("DELETE FROM surveys WHERE id = ?");
+        $stmt->execute($survey->getId());
+    }
+}
+
+?>
