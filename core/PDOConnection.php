926cbd388de2ca6e72e76b98c727bd7eddb6ff02
AlbertoLR <alber.lpz@gmail.com>
2018-10-21 18:57:15 +0200

    Add files via upload

--- core/PDOConnection.php
+++ core/PDOConnection.php
@@ -3,9 +3,9 @@
 
 class PDOConnection {
 	private static $dbhost = "127.0.0.1";
-	private static $dbname = "mvcblog";
-	private static $dbuser = "mvcuser";
-	private static $dbpass = "mvcblogpass";
+	private static $dbname = "bdtsw";
+	private static $dbuser = "kdamosuser";
+	private static $dbpass = "kdamospass";
 	private static $db_singleton = null;
 
 	public static function getInstance() {
