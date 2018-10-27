add9e495ec2f54e9a2613312d76a8821945035ad
AlbertoLR <alber.lpz@gmail.com>
2018-10-27 17:58:11 +0200

    Add files via upload

--- model/VoteMapper.php
+++ model/VoteMapper.php
@@ -1,7 +1,7 @@
  <?php
 require_once(__DIR__."/../core/PDOConnection.php");
 require_once(__DIR__."/../model/Vote.php");
-require_once(__DIR__."/../model/Option.php");
+require_once(__DIR__."/../model/Survey.php");
 require_once(__DIR__."/../model/User.php");
 
 class VoteMapper{
@@ -11,26 +11,42 @@ class VoteMapper{
         $this->db = PDOConnection::getInstance();
     }
 	
-	public function findByOptionId($option_id){
-		$stmt = $this->db->prepare("SELECT user_id , vote FROM votes WHERE option.id = ?");
-        $stmt->execute(array($option_id));
-        $options_row = $stmt->fetch(PDO::FETCH_ASSOC);
+	public function findOptionsBySurveyId($survey_id){
+		$stmt = $this->db->prepare("SELECT * FROM options WHERE survey_id = ? ORDER BY day, start");
+        $stmt->execute(array($survey_id));
+        $options_row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		
-		if($options_row == NULL) {
-            return NULL;
-        }
-	}
+		$options_by_survey = array();
+		foreach($options_row as $option){
+			array_push ($options_by_survey, new Option($option["id"], $survey_id, $option["day"], $option["start"], $option["end"]));
+		}
+		
+        return $options_by_survey;       
+	}	
 	
+/**	public function countByOptionId($option_id){
+		$stmt = $this->db->prepare("SELECT count(user_id) FROM votes WHERE option_id = ? AND vote = 'Y'");
+        $stmt->execute(array($option_id));
+        $counts_row = $stmt->fetchColumn();
+        return $counts_row;       
+	}
+*/	
 	
 	public function findBySurveyId($survey_id){
-		$stmt = $this->db->prepare("SELECT votes.user_id, votes.option_id, votes.vote, 
-		options.day, options.start, options.end FROM votes JOIN options ON options.id=votes.option_id WHERE options.survey_id = '?'");
+		$stmt = $this->db->prepare("SELECT votes.user_id, users.name, users.surname, votes.option_id, 
+		options.day, options.start, options.end, votes.vote FROM votes JOIN options ON options.id=votes.option_id JOIN users ON users.id=votes.user_id 
+		WHERE options.survey_id = ? ORDER BY votes.option_id, votes.user_id");
         $stmt->execute(array($survey_id));
-        $survey_row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $votes_row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		
-		if($survey_row == NULL) {
-            return NULL;
+		$votes_by_survey = array();
+		
+		foreach($votes_row as $vote) {
+            array_push ($votes_by_survey, new Vote(new User($vote["user_id"], $vote["name"], $vote["surname"], NULL, NULL), 
+										new Option($vote["option_id"], $survey_id, $vote["day"], $vote["start"], $vote["end"]),
+											$vote["vote"]));
         }
+		return $votes_by_survey;
 	}
 
 
@@ -42,4 +58,4 @@ class VoteMapper{
 	public function update(Vote $vote) {
         $stmt = $this->db->prepare("UPDATE votes SET vote=? where user_id =? and option_id=? ");
         $stmt->execute(array($vote->getVote(), $vote->getUserId() , $vote->getOptionId()));
-    }
+	} }
