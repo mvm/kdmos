5b58d12012aa8459d3aae25434dfcacc4e2a0b0b
Miguel Vicente <mvicmoure@gmail.com>
2018-10-28 10:08:59 +0100

    Translate messages

--- view/messages/messages_es.php
+++ view/messages/messages_es.php
@@ -48,7 +48,16 @@ array(
 	"Post \"%s\" successfully added." => "Artículo \"%s\" añadido.",
 	"Post \"%s\" successfully updated." => "Artículo \"%s\" modificado.",
 	"Post \"%s\" successfully deleted." => "Artículo \"%s\" eliminado.",
-
+    "Surveys" => "Encuestas",
+    "Vote" => "Votar",
+    "Go Back" => "Volver",
+    "Status/Update" => "Estado/Actualizar",
+    "Create Survey:" => "Crear Encuesta:",
+    "Create a new survey and send a link for voting to any registered friend." => "Crear una nueva encuesta y enviar enlace para votar a un amigo registrado.",
+    "Search for surveys:" => "Listar encuestas:",
+    "Search for surveys where you have voted for consulting its status and, if you want, update your vote" => "Listar encuestas donde has votado para consultar si estado, y si quieres, actualizar tu voto.",
+    "Search for your surveys:" => "Listar tus encuestas:",
+    "Searh for the surveys that you created for consulting its status and, if you want, modify the options." => "Listar encuestas que has creado para consultar su estado y, si quieres, modificar sus fechas.",
 
 	"Edit" => "Editar",
 	"Delete" => "Eliminar",
