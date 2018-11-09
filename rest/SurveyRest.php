28a52b0293a26b8337457fafb2c088940172eccc
Miguel Vicente <mvicmoure@gmail.com>
2018-11-09 10:32:33 +0100

    List surveys you've taken part of

--- rest/SurveyRest.php
+++ rest/SurveyRest.php
@@ -33,10 +33,29 @@ class SurveyRest extends BaseRest {
         header("Content-Type: application/json");
         echo(json_encode($result));
     }
+
+    public function getParticipated() {
+        $currentUser = parent::authenticateUser();
+        $surveys = $this->surveyMapper->findByParticipated($currentUser->getId());
+        $result = array();
+
+        foreach($surveys as $s) {
+            array_push($result, array(
+                "id" => $s->getId(),
+                "title" => $s->getTitle(),
+                "description" => $s->getDescription()
+            ));
+        }
+
+        header($_SERVER["SERVER_PROTOCOL"] . " 200 Ok");
+        header("Content-Type: application/json");
+        echo(json_encode($result));
+    }
 }
 
 $surveyRest = new SurveyRest();
 URIDispatcher::getInstance()
-    ->map("GET", "/survey/created", array($surveyRest, "getCreated"));
+    ->map("GET", "/survey/created", array($surveyRest, "getCreated"))
+    ->map("GET", "/survey/participated", array($surveyRest, "getParticipated"));
 
 ?>
