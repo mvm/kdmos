53fc27f47a3f0749b69cb8d49fd2ce1bc6ab472a
Miguel Vicente <mvicmoure@gmail.com>
2018-12-02 12:36:38 +0100

    Login in user bar

--- frontend/js/component/logincomponent.js
+++ frontend/js/component/logincomponent.js
@@ -1,23 +1,24 @@
 class LoginComponent extends Fronty.ModelComponent {
-  constructor(userModel, router) {
-    super(Handlebars.templates.login, userModel);
-    this.userModel = userModel;
-    this.userService = new UserService();
-    this.router = router;
+    constructor(userModel, router) {
+	super(Handlebars.templates.login, userModel);
+	this.userModel = userModel;
+	this.userService = new UserService();
+	this.router = router;
 
-    this.addEventListener('click', '#loginbutton', (event) => {
-      this.userService.login($('#login').val(), $('#password').val())
-        .then(() => {
-          this.router.goToPage('posts');
-          this.userModel.setLoggeduser($('#login').val());
-        })
-        .catch((error) => {
-          this.userModel.set((model) => {
-            model.loginError = error.responseText;
-          });
-          this.userModel.logout();
-        });
-    });
+	this.addEventListener('click', '#loginbutton', (event) => {
+	    this.userService.login($('#login').val(), $('#password').val())
+		.then(() => {
+		    this.router.goToPage('posts');
+		    this.userModel.setLoggeduser($('#login').val());
+		    
+		})
+		.catch((error) => {
+		    this.userModel.set((model) => {
+			model.loginError = error.responseText;
+		    });
+		    this.userModel.logout();
+		});
+	});
 
     this.addEventListener('click', '#registerlink', () => {
       this.userModel.set(() => {
