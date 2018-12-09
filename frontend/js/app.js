9410227fff74922a9c33b9d226e1ed59083af8bc
AlbertoLR <alber.lpz@gmail.com>
2018-12-09 01:58:11 +0100

    Added votes

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -39,6 +39,8 @@ Promise.all([
       Handlebars.templates.surveyrow = Handlebars.compile(source)),
     loadTextFile('templates/components/option.hbs').then((source) =>
       Handlebars.templates.option = Handlebars.compile(source)),
+		loadTextFile('templates/components/votes.hbs').then((source) =>
+      Handlebars.templates.votes = Handlebars.compile(source)),
     loadTextFile('templates/components/posts-table.hbs').then((source) =>
       Handlebars.templates.poststable = Handlebars.compile(source)),      
     loadTextFile('templates/components/post-edit.hbs').then((source) =>
