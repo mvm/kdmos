997a5d941587d40cb7fd93807c789a0005f3eb66
Miguel Vicente <mvicmoure@gmail.com>
2018-12-09 11:07:58 +0100

    Fetch survey from REST API

--- frontend/js/component/surveyeditcomponent.js
+++ frontend/js/component/surveyeditcomponent.js
@@ -7,6 +7,20 @@ class SurveyEditComponent extends Fronty.ModelComponent {
 	this.router = router;
     }
 
+    onStart() {
+	var id = this.router.getRouteQueryParam('id');
+	if(id != null) {
+	    this.surveysService.findSurvey(id)
+		.then((survey) => {
+		    this.surveyModel.set( (self) => {
+			self.id = survey.id;
+			self.title = survey.title;
+			self.description = survey.description;
+		    });
+		});
+	}
+    }
+
     createChildModelComponent(className, element, id, modelItem) {
 	return new OptionComponent(modelItem, this);
     }
